9bc51e830d7cedfb818f36854e45a72e326fe75b
Sanjanacodes <sanjanamukherjee1997@gmail.com>
2025-06-17 16:34:30 +0530

    2nd push

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from "react"; // added after CDN links are removed
+import ReactDOM from "react-dom/client"; // added after CDN links are removed (/client used for React 19)
 const parent = React.createElement(
     "div",
     {id : "parent"},
     React.createElement(
         "div",
         {id : "child"},
-        [React.createElement("h1",{},"Hello1"), React.createElement("h2",{},"Hello2")]
+        [React.createElement("h1",{},"Hello123"), React.createElement("h2",{},"Hello2")]
     )
 );
 //const heading = React.createElement("h1",{},"Hello");
